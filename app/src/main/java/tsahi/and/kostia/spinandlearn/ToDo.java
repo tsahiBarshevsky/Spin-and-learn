aae30d49d9bcefdbe0bf61b2eee67d04d34252f1
kostya <kostya@kl>
2019-02-04 12:58:16 +0200

    hebrew tutorial fit fix

--- app/src/main/java/tsahi/and/kostia/spinandlearn/ToDo.java
+++ app/src/main/java/tsahi/and/kostia/spinandlearn/ToDo.java
@@ -3,7 +3,6 @@ package tsahi.and.kostia.spinandlearn;
 public @interface ToDo {
 
     //todo localization to russian
-    //todo confetti https://github.com/DanielMartinus/Konfetti
     //todo spin button animation after bonus
     //todo front facing camera
 }
